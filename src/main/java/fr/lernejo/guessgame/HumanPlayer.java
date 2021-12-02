b8cc51385461f3565ab3cf12638161406d583f9f
Amina Arioui <arioui@esiea.fr>
2021-12-02 14:29:21 +0100

    part 3

---
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -0,0 +1,25 @@
+package fr.lernejo.guessgame;
+
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+
+import java.util.Scanner;
+
+public class HumanPlayer implements Player {
+    Logger logger = LoggerFactory.getLogger("Player");
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public long askNextGuess() {
+        return scanner.nextInt();
+    }
+
+    @Override
+    public void respond(boolean lowerOrGreater) {
+        if (lowerOrGreater){
+            System.out.println("Le nombre est le plus grand");
+        } else {
+            System.out.println("Le nombre estv plus petit");
+        }
+    }
+}
